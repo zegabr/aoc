3afb325ccf6e7c51efc4df99852b16681ba1fcde
Jose Gabriel Silva Pereira <jgsp2@ug2c45.windows.cin.ufpe.br>
2018-12-20 10:37:27 -0300

    day5 part 2 done but unoptimized

--- AoC/dec2018/d5/b.py
+++ AoC/dec2018/d5/b.py
@@ -1,2 +1,23 @@
 import sys
+from string import ascii_lowercase
 s=sys.stdin.read()
+mini=len(s)
+
+for c in ascii_lowercase:
+    t=s.replace(c,"")
+    t=t.replace(c.upper(),"")
+
+    cont = 1 ## bool para continuar a operacao
+    while(cont):
+        cont=0
+        i=0
+        for i in range(len(t)-1):
+            j=i+1
+            if t[i].lower()==t[j].lower() and t[i]!=t[j]:
+                t=t.replace(t[i]+t[j],"")
+                cont=1
+                break
+ 
+    mini=min([mini,len(t)])
+            
+print(mini)
